e6213d4a3df2f23221b72e9db312d74d7c23854a
Pawel Bacza <bacza2@gmail.com>
2025-11-26 17:17:35 +0000

    Add memory features

--- src/mastra/agents/framework-agent.ts
+++ src/mastra/agents/framework-agent.ts
@@ -1,44 +1,40 @@
 import { Agent } from "@mastra/core/agent";
 import { Memory } from "@mastra/memory";
-import { LibSQLStore } from "@mastra/libsql";
+import { LibSQLStore, LibSQLVector } from "@mastra/libsql";
+import { fastembed } from "@mastra/fastembed";
 
 import { readFileTool } from "../tools/read-file";
 import { readDirectoryTool } from "../tools/read-directory";
 import { writeFileTool } from "../tools/write-file";
 import { deleteFileTool } from "../tools/delete-file";
-import {
-  completenessScorer,
-  toolCallAppropriatenessScorer,
-} from "../scorers/file-scorer";
 
 export const frameworkAgent = new Agent({
   name: "Framework Agent",
   instructions: `
       You are a helpful assistant that can operate on file.
-
       When you are asked for something first consider if you could achieve it by doing something with files.
-
-      You have access to ../../recipes folder
+      You have access to ../../recipes folder.
+      If you will find relevant information story it in memory
   `,
   model: "openai/gpt-4.1-mini",
   tools: { readFileTool, readDirectoryTool, writeFileTool, deleteFileTool },
-  scorers: {
-    toolCallAppropriateness: {
-      scorer: toolCallAppropriatenessScorer,
-      sampling: {
-        type: "ratio",
-        rate: 1,
-      },
-    },
-    completeness: {
-      scorer: completenessScorer,
-      sampling: {
-        type: "ratio",
-        rate: 1,
-      },
-    },
-  },
   memory: new Memory({
+    options: {
+      lastMessages: 10,
+      semanticRecall: {
+        topK: 1,
+        messageRange: 1,
+        scope: "resource",
+      },
+      workingMemory: {
+        enabled: true,
+        template: `preferred folder for operations: `,
+      },
+    },
+    embedder: fastembed,
+    vector: new LibSQLVector({
+      connectionUrl: "file:../vect.db",
+    }),
     storage: new LibSQLStore({
       url: "file:../mastra.db", // path is relative to the .mastra/output directory
     }),
--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -23,7 +23,7 @@ export const mastra = new Mastra({
   }),
   telemetry: {
     // Telemetry is deprecated and will be removed in the Nov 4th release
-    enabled: false,
+    enabled: true,
   },
   observability: {
     // Enables DefaultExporter and CloudExporter for AI tracing
